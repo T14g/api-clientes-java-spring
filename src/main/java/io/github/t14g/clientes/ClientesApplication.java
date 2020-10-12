f5a37e43e444c09bbefc24ef1f53a87e9f83861b
Tiago <ottiagos@gmail.com>
2020-10-12 19:56:22 -0300

    Models e repositories

--- src/main/java/io/github/t14g/clientes/ClientesApplication.java
+++ src/main/java/io/github/t14g/clientes/ClientesApplication.java
@@ -1,13 +1,30 @@
 package io.github.t14g.clientes;
 
+import io.github.t14g.clientes.model.entity.Cliente;
+import io.github.t14g.clientes.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 //Classe que inicializa Na raiz , nome do projeto + Application no final
 //Essa anotation manda o springboot usar essa classe para inicializar a aplicação
 //Essa anotation também configura toda injeção de dependências
 @SpringBootApplication
 public class ClientesApplication {
+
+    //Injeção de dep no parâmetro do método
+    //Código executado assim que inicializar a aplicação por ter @Bean
+    @Bean
+    public CommandLineRunner run(@Autowired ClienteRepository repository){
+        return  args -> {
+            //O builder vai preenchendo como se fosse 1 formulário
+            Cliente cliente = Cliente.builder().cpf("99999999999").nome("Fulano").build();
+            repository.save(cliente);
+        };
+    }
+
     public static void main(String[] args) {
         //Incializa a aplicação
         SpringApplication.run(ClientesApplication.class, args);
--- src/main/java/io/github/t14g/clientes/model/entity/Cliente.java
+++ src/main/java/io/github/t14g/clientes/model/entity/Cliente.java
@@ -1,15 +1,23 @@
 package io.github.t14g.clientes.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 //Para dizer que é uma Entidade JPA
 @Entity
+//Construtores com e sem parâmetros gerados pelo Lombock
+@NoArgsConstructor
+@AllArgsConstructor
+//Builder de cliente
 //Com lombock em tempo de compilação tem os setters mad em Dev tem só a notation
 //@Getter@Setter // vai gerar getters e setters automaticamente
 @Data// Cria get e setters, hashing,entre outros
+@Builder
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO) // o banco auto incrementa o ID
@@ -23,5 +31,11 @@ public class Cliente {
 
     @Column(name = "data_cadastro") //Em bancos usar _
     private LocalDate dataCadastro;
+
+    //Na hora que for persistir coloca a data atual, antes de persistir na verdade
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+    }
 
 }
