2031fc993c3c15340e91ea75dc9b32cff8ad50bf
Tiago <ottiagos@gmail.com>
2020-11-14 17:16:13 -0300

    Js hello

--- src/main/java/io/github/t14g/clientes/clientes-app/src/app/hello/hello.component.ts
+++ src/main/java/io/github/t14g/clientes/clientes-app/src/app/hello/hello.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ClientesComponent } from '../clientes/clientes.component';
 
 @Component({
     selector: 'hello',
-    template: `
-        <h1>Hello World </h1>
-    `
+    templateUrl: './hello.component.html'
 })
  
 export class HelloComponent {
 
+    nome : string;
+    clientes : Cliente[];
+
+    constructor(){
+        this.nome  = 'Declaração de variável no typescript';
+        let fulano = new Cliente('Fulano',20);
+        let ciclano = new Cliente('Ciclano',40);
+
+        this.clientes = [fulano, ciclano];
+    }
+
+}
+
+class Cliente {
+
+    constructor(
+        public nome : string,
+        public idade: number
+    ){}
 }
